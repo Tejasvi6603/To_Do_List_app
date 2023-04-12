5820a090cd7267bbe97aa5a308da65c48d686634
Tejasvi6603 <123891856+Tejasvi6603@users.noreply.github.com>
2023-04-12 17:03:51 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This ia an about MyComponents;
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error fugiat repellat aperiam, corporis suscipit dolorem excepturi ratione vero nostrum iste. Natus blanditiis praesentium, magni dignissimos officia vero error. Neque, dolore blanditiis! Quibusdam, deleniti incidunt?</p>
+    </div>
+  )
+}
---
+++ Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Footer = () => {
+  // let footerstyle={
+  //   position:"absolute",
+  //   top:"100vh",
+  //   width:"100%",
+  // }
+  return (
+    <footer className="bg-dark text-light py-3" >
+      <p className="text-center my-6"></p>
+      Copyright &copy; MyTodoList.com</footer>
+  )
+}
